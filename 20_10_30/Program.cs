3f6ee73fdfba4fb9187b0be36755f12bed28db76
RyuJoonSik <tldtksahtxja@naver.com>
2020-10-30 16:34:19 +0900

    CSharp Grammar

---
+++ 20_10_30/Program.cs
@@ -0,0 +1,13 @@
+﻿using System; // System 라이브러리 사용(System 네임스페이스의 모든 클래스를 사용 가능)
+              // 네임스페이스 : 명칭을 저장하는 영역. 주로 클래스를 담음
+
+namespace _20_10_30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
